b0fe4e52ad2aa2343eac7899bf3759842e21ee3d
yorkglez <bobo_12yors@hotmail.com>
2018-03-16 20:27:48 -0600

    1

--- resources/views/auth/structurelogin.blade.php
+++ resources/views/auth/structurelogin.blade.php
@@ -10,7 +10,7 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 </head>
 <body>
-  {{-- Body nav --}}
+  {{-- Body nav- --}}
   <nav class="navbar navbar-expand-lg  navbar-light bg-light">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
